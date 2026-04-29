a0f6066ba86492e5ef4e6fc0f77a0b2d68f943eb
Andy <andymucyo@gmail.com>
2026-04-29 19:44:29 +0200

    refactor: remove_song method to take index as argument

--- src/modules/MusicDatabase.py
+++ src/modules/MusicDatabase.py
@@ -17,15 +17,12 @@ class MusicDatabase:
     def add_song(self, music):
         self.data = pd.concat([self.data, pd.DataFrame([music])], ignore_index=True)
 
-    def remove_song(self, music: str):
-        if "Title" not in self.data.columns:
-            print("Title column not found")
-            return
-        music_index = self.data.loc[self.data["Title"] == music].index
-        if music_index.empty:
-            print("Music not found")
-            return
-        self.data = self.data.drop(music_index).reset_index(drop=True)
+    def remove_song(self, index):
+        try:
+            self.data = self.data.drop(index)
+            self.data = self.data.reset_index(drop=True)
+        except KeyError:
+            raise KeyError(f"Index entry: {index}, not valid. Cannot remove song")
 
     def save(self, filepath):
         if filepath:
