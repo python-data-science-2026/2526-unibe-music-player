574683219953e4cc07fdc4293d630f1e226b6fd5
Andy <andymucyo@gmail.com>
2026-04-30 12:37:00 +0200

    enhancement: added both method to remove song (str and index)

--- src/modules/MusicDatabase.py
+++ src/modules/MusicDatabase.py
@@ -17,7 +17,17 @@ class MusicDatabase:
     def add_song(self, music):
         self.data = pd.concat([self.data, pd.DataFrame([music])], ignore_index=True)
 
-    def remove_song(self, index):
+    def remove_song_by_name(self, music: str):
+        if "Title" not in self.data.columns:
+            print("Title column not found")
+            return
+        music_index = self.data.loc[self.data["Title"] == music].index
+        if music_index.empty:
+            print("Music not found")
+            return
+        self.data = self.data.drop(music_index).reset_index(drop=True)
+
+    def remove_song_by_index(self, index):
         try:
             self.data = self.data.drop(index)
             self.data = self.data.reset_index(drop=True)
